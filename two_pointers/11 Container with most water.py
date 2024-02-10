0fba95b852b6cff5c67598432b07b5de2558742d
gauravgpta93 <57009145+gauravgpta93@users.noreply.github.com>
2024-02-10 00:13:20 -0500

    Added a couple more problems on two pointers

--- two_pointers/11 Container with most water.py
+++ two_pointers/11 Container with most water.py
@@ -4,12 +4,15 @@ class Solution:
         max_hold = 0
         while start_index < end_index:
             start_height, end_height = height[start_index], height[end_index]
-            max_hold = max(max_hold, min(start_height, end_height) * (end_index - start_index))
+            max_hold = max(
+                max_hold, min(start_height, end_height) * (end_index - start_index)
+            )
             if start_height < end_height:
                 start_index += 1
             else:
                 end_index -= 1
         return max_hold
+
 
 def main():
     answer = Solution()
@@ -17,7 +20,8 @@ def main():
     assert answer.maxArea([1, 1]) == 1
     assert answer.maxArea([4, 3, 2, 1, 4]) == 16
     assert answer.maxArea([1, 2, 1]) == 2
-    print('All test cases passed.')
+    print("All test cases passed.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
